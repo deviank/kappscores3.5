28ff13a3f0666dab329dc6dfe9457ec2bbed48ce
unknown <forza9882@gmail.com>
2021-01-07 18:06:56 +0200

    Modifying custom loop and testing output.

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php get_header();?>
+<pre>
+<?php var_dump($wp_query)?>
+</pre>
     <?php if (have_posts()) { ?>
         <?php while (have_posts()){ ?>
             <?php the_post();?>
@@ -13,7 +16,9 @@
                 By <a href="<?php echo get_author_posts_url(get_the_author_meta('ID'))?>"><?php echo get_the_author();?></a>
             </div>
             <div><?php the_excerpt();?></div>
+            <a href="<?php echo get_the_permalink()?>" title="<?php the_title_attribute()?>">Read More <span class="u-screen-reader-text">About <?php the_title()?></span></a>
             <?php } ?>
+            <?php the_posts_pagination()?>
         <?php } else { ?>
         <p>Sorry, there are no posts that match your criteria</p>
         <?php } ?>
