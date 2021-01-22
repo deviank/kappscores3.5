27d61a9cce03c214c484b93b1c82060ac1817339
unknown <forza9882@gmail.com>
2021-01-22 11:41:13 +0200

    Creating gulpfile.js for tasks.

---
+++ gulpfile.babel.js
@@ -0,0 +1,16 @@
+/* var gulp = require('gulp');
+
+gulp.task('default', defaultTask);
+
+function defaultTask(done){
+    console.log('hey')
+    done()
+} */
+
+import gulp from 'gulp';
+
+export const hello = (done) => {
+    console.log(hello);
+    done();
+
+}
