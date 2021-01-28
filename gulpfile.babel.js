4e641d584eada0d9734e5177ac3359b07ee9ac36
unknown <forza9882@gmail.com>
2021-01-28 15:51:32 +0200

    Adding source files.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,16 +1,13 @@
-/* var gulp = require('gulp');
-
-gulp.task('default', defaultTask);
-
-function defaultTask(done){
-    console.log('hey')
-    done()
-} */
-
 import gulp from 'gulp';
+import yargs from 'yargs';
+import sass from 'gulp-sass';
+
+const PRODUCTION = yargs.argv.prod;
 
 export const hello = (done) => {
-    console.log(hello);
+    console.log(PRODUCTION);
     done();
 
 }
+
+//export default hello;
