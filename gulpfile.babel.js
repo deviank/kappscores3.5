f0e92e015bf1007b9f6418cc7e908ab0b6d6457d
unknown <forza9882@gmail.com>
2021-01-30 11:50:29 +0200

    Adding minifying components for css.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,12 +1,14 @@
 import gulp from 'gulp';
 import yargs from 'yargs';
 import sass from 'gulp-sass';
+import cleanCSS from 'gulp-clean-css'
 
 const PRODUCTION = yargs.argv.prod;
 
 export const styles = () => {
     return gulp.src('src/assets/scss/bundle.scss')
     .pipe(sass().on('error', sass.logError))
+    .pipe(cleanCSS({compatibility: 'ie8'}))
     .pipe(gulp.dest('dist/assets/css'));
 
 }
