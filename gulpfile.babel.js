b345da50cce84c1a3b0db7f56c30ec80b05f1b59
unknown <forza9882@gmail.com>
2021-02-04 17:45:46 +0200

    Adding admin.scss file.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -8,7 +8,7 @@ import sourcemaps from 'gulp-sourcemaps'
 const PRODUCTION = yargs.argv.prod;
 
 export const styles = () => {
-    return gulp.src('src/assets/scss/bundle.scss')
+    return gulp.src(['src/assets/scss/bundle.scss'])
     .pipe(gulpif(!PRODUCTION, sourcemaps.init()))
     .pipe(sass().on('error', sass.logError))
     .pipe(gulpif(PRODUCTION, cleanCSS({compatibility: 'ie8'})))
