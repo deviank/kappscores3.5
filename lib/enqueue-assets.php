369009c6c0e55193b3ed948bcfa1d7a9264299f8
unknown <forza9882@gmail.com>
2021-02-04 18:00:39 +0200

    Adding admin style scripts to load conditionally for admin area.

--- lib/enqueue-assets.php
+++ lib/enqueue-assets.php
@@ -4,4 +4,11 @@ function kappscores_assets(){
 }
 
 add_action( 'wp_enqueue_scripts', 'kappscores_assets');
+
+function kappscores_admin_assets(){
+    wp_enqueue_style( 'kappscores-admin-stylesheet', get_template_directory_uri() . '/dist/assets/css/admin.css', array(), '1.0.0', 'all' );
+}
+
+add_action( 'admin_enqueue_scripts', 'kappscores_admin_assets');
+
 ?>
