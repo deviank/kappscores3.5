319dc7cb29ef466a47110ce5dce61d27189aa52e
unknown <forza9882@gmail.com>
2021-05-11 18:25:00 +0200

    Removing do_action test function from functions.php.

--- functions.php
+++ functions.php
@@ -6,13 +6,8 @@ require_once('lib/theme-support.php');
 require_once('lib/navigation.php');
 ?>
 
+
 <?php
-function after_pagination(){
-    echo 'This is a do_action_test';
-}
-add_action( '_themename_after_pagination', 'after_pagination');
-
-
 function no_post_text($text){
     return $text ;
 }
