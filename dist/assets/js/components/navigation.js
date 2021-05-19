c64c75e7f058c44ea739e20e6b3b3eccd60239ae
unknown <forza9882@gmail.com>
2021-05-19 18:55:02 +0200

    Fixing issue where previous menu items remain open after moving to next menu item.

--- dist/assets/js/components/navigation.js
+++ dist/assets/js/components/navigation.js
@@ -1,7 +1,22 @@
-import $ from 'jquery'
+import $ from 'jquery';
 
 $('.c-navigation').on('mouseenter', '.menu-item-has-children', (e) => {
     $(e.currentTarget).addClass('open');
 }).on('mouseleave', '.menu-item-has-children', (e) => {
     $(e.currentTarget).removeClass('open');
+})
+
+$('.c-navigation').on('click', '.menu .menu-button', (e) => {
+    e.preventDefault();
+    let menu_button = $(e.currentTarget);
+    let menu_link = menu_button.parent();
+    let menu_item = menu_link.parent();
+
+    if(menu_item.hasClass('open')){
+
+    } else {
+        menu_item.addClass('open');
+
+    }
+
 })
