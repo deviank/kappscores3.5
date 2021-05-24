f129f479d537b5f97654bc8c7fa012c9caf4c266
unknown <forza9882@gmail.com>
2021-05-24 17:43:04 +0200

    Adding skip links for accessability.

--- dist/assets/js/components/navigation.js
+++ dist/assets/js/components/navigation.js
@@ -1,6 +1,9 @@
 import $ from 'jquery';
 
 $('.c-navigation').on('mouseenter', '.menu-item-has-children', (e) => {
+    if(!$(e.currentTarget).parents('.sub-menu').length){
+        $('.menu > .menu-item.open').find('> a > .menu-button').trigger('click');
+    }
     $(e.currentTarget).addClass('open');
 }).on('mouseleave', '.menu-item-has-children', (e) => {
     $(e.currentTarget).removeClass('open');
@@ -10,6 +13,7 @@ $('.c-navigation').on('mouseenter', '.menu-item-has-children', (e) => {
 
 $('.c-navigation').on('click', '.menu .menu-button', (e) => {
     e.preventDefault();
+    e.stopPropagation();
     let menu_button = $(e.currentTarget);
     let menu_link = menu_button.parent();
     let menu_item = menu_link.parent();
@@ -20,7 +24,7 @@ $('.c-navigation').on('click', '.menu .menu-button', (e) => {
         menu_button.find('.menu-button-show').attr('aria-hidden', 'false');
         menu_button.find('.menu-button-hide').attr('aria-hidden', 'true');
     } else {
-        menu_item.siblings('.open').find('>a>.menu-button').trigger('click')
+        menu_item.siblings('.open').find('> a >.menu-button').trigger('click')
         menu_item.addClass('open');
         menu_link.attr('aria-expanded', 'true');
         menu_button.find('.menu-button-show').attr('aria-hidden', 'true');
@@ -28,4 +32,10 @@ $('.c-navigation').on('click', '.menu .menu-button', (e) => {
 
     }
 
-})
+});
+
+$(document).click((e) => {
+    if($('.menu-item.open').length){
+        $('.menu > .menu-item.open > a > .menu-button').trigger('click');
+    }
+})                              
