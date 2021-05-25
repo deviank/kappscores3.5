a0edc98b147d8e2f17237ebc000b2c84c2add752
unknown <forza9882@gmail.com>
2021-05-25 18:41:14 +0200

    Adding Footer Options section in customizer.

--- lib/customize.php
+++ lib/customize.php
@@ -4,7 +4,8 @@ function _themename_customize_register($wp_customize){
 
     $wp_customize->add_section('_themename_footer_options', array(
         'title' => esc_html__( 'Footer Options', '_themename'),
-        'description' => esc_html__( 'You can change footer options from here', '_themename' )
+        'description' => esc_html__( 'You can change footer options from here', '_themename' ),
+        
     ));
 
     $wp_customize->add_setting('_themename_site_info', array(
@@ -15,7 +16,7 @@ function _themename_customize_register($wp_customize){
     $wp_customize->add_control('_themename_site_info', array(
         'type' => 'text',
         'label' => esc_html__( 'Site Info', '_themename'),
-        'section' => 'title_tagline'
+        'section' => '_themename_footer_options'
 
     ));
 
