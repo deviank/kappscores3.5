14d616b2473e5a11dc079749719fca03e2a5613b
unknown <forza9882@gmail.com>
2021-06-28 18:29:10 +0200

    Adding custom color picker.

--- lib/enqueue-assets.php
+++ lib/enqueue-assets.php
@@ -2,6 +2,8 @@
 function _themename_assets(){
     wp_enqueue_style( '_themename-stylesheet', get_template_directory_uri() . '/dist/assets/css/bundle.css', array(), '1.0.0', 'all' );
 
+    wp_add_inline_style( '_themename-stylesheet', '' );
+
     wp_enqueue_script( '_themename-scripts', get_template_directory_uri(  ) .  '/dist/assets/js/bundle.js', array('jquery'), '1.0.0', true);
 }
 
