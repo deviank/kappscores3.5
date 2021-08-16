c2c108a4ea84a6e1a0bb1c5724d3650ac5a5c746
unknown <forza9882@gmail.com>
2021-08-16 18:19:25 +0200

    Updating index page.

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
             <?php if(is_active_sidebar( 'primary-sidebar' )){?>
                 <div class="o-row__column o-row__column--span-12 o-row__column--span-4@medium">
                     <?php get_sidebar();?>
+                    
                 </div>
             <?php } ?>
         </div>
