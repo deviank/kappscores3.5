7e9245d6d1fab5ca36b3cb0929d2997a880145fe
Devian Kapp <forza9882@gmail.com>
2021-11-08 18:13:55 +0200

    Adding gulp task to bundle and zip theme files.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -121,7 +121,7 @@ export const watch = () => {
 
 export const compress = () => {
     return gulp.src(paths.package.src)
-        .pipe(replace('_themename', info.name))
+        .pipe(gulpif((file) => (file.relative.split('.').pop() !== 'zip'), replace('_themename', info.name)))
         .pipe(zip(`${info.name}.zip`))
         .pipe(gulp.dest(paths.package.dest));
 }
