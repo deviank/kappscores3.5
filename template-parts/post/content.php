dfc9477b84542a84950d169ef95cbe464cd55d7d
Devian Kapp <forza9882@gmail.com>
2021-11-24 06:01:58 +0200

    Populating single post template with additional meta and content.

--- template-parts/post/content.php
+++ template-parts/post/content.php
@@ -1,5 +1,12 @@
-<article
-    <?php post_class("c-post u-margin-bottom-20");?>>
+<article <?php post_class("c-post u-margin-bottom-20");?>>
+    <div class="c-post__inner">
+
+            <?php if(get_the_post_thumbnail() !== '') {?>
+                <div class="c-post__thumbnail">
+                    <?php the_post_thumbnail('large');?>
+                </div>
+            <?php } ?>
+            
         <h2 class="c-post__title">
             <a href="<?php the_permalink()?>"><?php the_title()?></a>
         </h2>
@@ -14,4 +21,6 @@
             <?php echo _themename_delete_post();?>
             <?php var_dump(get_post_meta( get_the_ID(), 'price' ));?>
         </div>
+    </div>
+    
 </article>
