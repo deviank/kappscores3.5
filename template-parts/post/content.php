fb939906a530b816701c1b1fc4c4e6a25a6f68fc
Devian Kapp <forza9882@gmail.com>
2021-11-26 05:55:36 +0200

    Adding category and tags in footer of single post template.

--- template-parts/post/content.php
+++ template-parts/post/content.php
@@ -1,26 +1,30 @@
-<article <?php post_class("c-post u-margin-bottom-20");?>>
+<article <?php post_class('c-post u-margin-bottom-20'); ?>>
+    
     <div class="c-post__inner">
 
-            <?php if(get_the_post_thumbnail() !== '') {?>
-                <div class="c-post__thumbnail">
-                    <?php the_post_thumbnail('large');?>
-                </div>
-            <?php } ?>
-            
-        <h2 class="c-post__title">
-            <a href="<?php the_permalink()?>"><?php the_title()?></a>
-        </h2>
-        <div class="c-post__meta">
-            <?php _themename_post_meta()?>
-        </div>
-        <div class="c-post__excerpt">
-            <?php the_excerpt();?>
-        </div>
-        <div>
-            <?php _themename_readmore_link();?>
-            <?php echo _themename_delete_post();?>
-            <?php var_dump(get_post_meta( get_the_ID(), 'price' ));?>
-        </div>
+        <?php if(get_the_post_thumbnail() !== '') { ?>
+            <div class="c-post__thumbnail">
+                <?php the_post_thumbnail( '_themename-blog-image' ); ?>
+            </div>
+        <?php } ?>
+        
+        <?php get_template_part('template-parts/post/header'); ?>
+        
+        <?php if(is_single( )) { ?>
+            <div class="c-post__content">
+                <?php the_content(); 
+                wp_link_pages();
+                ?>
+            </div>
+        <?php } else { ?>
+            <div class="c-post__excerpt">
+                <?php the_excerpt(); ?>
+            </div>
+        <?php } ?>
+
+        <?php if(is_single( )) { ?>
+            <?php get_template_part('template-parts/post/footer'); ?>
+        <?php } ?>
+        <?php if(!is_single()) { _themename_readmore_link(); } ?>
     </div>
-    
 </article>
