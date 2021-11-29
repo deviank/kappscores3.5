5f58834e42f29ab7dcbc59c077c629f3c92677ac
Devian Kapp <forza9882@gmail.com>
2021-11-29 19:02:15 +0200

    Adding checkbox function to add to single posts.

--- single.php
+++ single.php
@@ -14,6 +14,7 @@ if($layout === 'sidebar' && !$sidebar) {
                         <?php while (have_posts()){ ?>
                             <?php the_post();?>
                             <?php get_template_part( 'template-parts/post/content' );?>
+                            
                             <?php get_template_part( 'template-parts/single/author' );?>
                         <?php }?>
                         
