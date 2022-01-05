19b411816ce292313e80a631e4efb842fb95ebb7
Devian Kapp <forza9882@gmail.com>
2022-01-05 06:11:03 +0200

    Adding shortcode function for plugin folder to test output for button markup.

--- functions.php
+++ functions.php
@@ -10,3 +10,13 @@ require_once('lib/include-plugins.php');
 require_once('lib/comment-callback.php');
 
 //require_once('lib/metaboxes.php');
+
+function _themename_button($atts = [], $content = null, $tag = '') {
+    extract(shortcode_atts([
+        'color' => 'red',
+        'text' => 'text'
+    ], $atts, $tag));
+    
+    return '<button class="_themename_button" style="background-color: ' . esc_attr($color) . '">' . do_shortcode($content) . '</button>';
+}
+add_shortcode('_themename_button', '_themename_button');
