9ce99a12751100d5440e20bea5163fcb565cb91e
Devian Kapp <forza9882@gmail.com>
2022-02-02 05:52:43 +0200

    Removing empty p tags from content loop.

--- page-templates/page-blank.php
+++ page-templates/page-blank.php
@@ -8,6 +8,36 @@ get_header(); ?>
     <?php while(have_posts()) { the_post(); ?>
         <article <?php post_class(); ?>>
             <?php the_content(); ?>
+            <?php
+            // $c = get_the_content();
+            // echo 'Initial <br />';
+            // echo $c;
+            // echo '<br><br>';
+
+            // $c = wpautop($c);
+            // echo 'wpautop <br>';
+            // echo esc_html($c);
+            // echo '<br><br>';
+
+            // $c = shortcode_unautop($c);
+            // echo 'shortcode_unautop <br>';
+            // echo esc_html($c);
+            // echo '<br /><br />';
+
+            // $c = do_shortcode($c);
+            // echo 'do_shortcode <br>';
+            // echo esc_html($c);
+            // echo '<br /><br />';
+
+            // $c = force_balance_tags($c);
+            // echo 'force_balance_tags <br>';
+            // echo esc_html($c);
+            // echo '<br /><br />';
+
+            // $c = preg_replace('#<p>(\s|&nbsp;)*+(<br\s*/*>)*(\s|&nbsp;)*</p>#i', '', ($c));
+            // echo 'preg_replace <br>';
+            // echo esc_html($c);
+            ?>
         </article>
     <?php } ?>
 </main>
