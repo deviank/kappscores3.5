ec4fb14b7f4416c21a88d66df0da044384b0550e
Devian Kapp <forza9882@gmail.com>
2022-02-03 05:33:01 +0200

    Adding theme support for post types.

--- lib/theme-support.php
+++ lib/theme-support.php
@@ -1,17 +1,27 @@
 <?php
 
-function _themename_theme_support(){
+function _themename_theme_support() {
     add_theme_support( 'title-tag' );
     add_theme_support( 'post-thumbnails' );
-    add_theme_support( 'title-tag', array('search-form', 'comment-form', 'gallery', 'caption') );
-    add_theme_support( 'cusomize-selective');
+    add_theme_support( 'automatic-feed-links' );
+    add_theme_support( 'html5', array('search-form', 'comment-list', 'comment-form', 'gallery', 'caption') );
+    add_theme_support( 'customize-selective-refresh-widgets' );
     add_theme_support( 'custom-logo', array(
         'height' => 200,
         'width' => 600,
         'flex-height' => true,
-        'flex-width' => true,
+        'flex-width' => true
     ) );
-    add_theme_support('align-wide');
+    add_theme_support('post-formats', array(
+        'aside',
+		'image',
+		'video',
+		'quote',
+		'link',
+		'gallery',
+		'audio',
+    ));
+    add_theme_support( 'align-wide' );
     add_image_size('_themename-blog-image', 1200, 0);
 }
 
