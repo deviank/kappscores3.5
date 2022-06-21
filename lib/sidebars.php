0a90a251da6f140f93a172ec4c1caef94c037609
Devian Kapp <forza9882@gmail.com>
2022-06-21 05:24:17 +0200

    Fixing issue with sidebar widgets styling.

--- lib/sidebars.php
+++ lib/sidebars.php
@@ -1,41 +1,38 @@
 <?php
 
-function _themename_sidebar_widgets(){
-    register_sidebar(array(
+function _themename_sidebar_widgets() {
+    register_sidebar( array(
         'id' => 'primary-sidebar',
-        'name' => esc_html__( 'Primary Sidebar', '_themename'),
-        'description' => esc_html__( 'This sidebar appears in the blog posts page', '_themename'),
-        'before_widget' => '<section id="%1s" class="c-sidebar-widget u-margin-bottom-20 %2s">',
-        'after-widget' => '</section>',
-        'before-title' =>  '<h5>',
-        'after-title' => '</h5>'
-    ));
+        'name' => esc_html__( 'Primary Sidebar', '_themename' ),
+        'description' => esc_html__( 'This sidebar appears in the blog posts page.', '_themename' ),
+        'before_widget' => '<section id="%1$s" class="c-sidebar-widget u-margin-bottom-20 %2$s">',
+        'after_widget' => '</section>',
+        'before_title' => '<h5>',
+        'after_title' => '</h5>'
+    ) );
 }
 
 $footer_layout = sanitize_text_field(get_theme_mod('_themename_footer_layout', '3,3,3,3'));
 $footer_layout = preg_replace('/\s+/', '', $footer_layout);
 $columns = explode(',', $footer_layout);
-$footer_bg = get_theme_mod( '_themename_footer_bg', 'dark' );
+$footer_bg = _themename_sanitize_footer_bg(get_theme_mod( '_themename_footer_bg', 'dark' ));
 $widget_theme = '';
-if($footer_bg == 'light'){
-    $widget_theme = 'c-footer--dark';
-}  else {
-    $widget_theme = 'c-footer--light';
+if($footer_bg == 'light') {
+    $widget_theme = 'c-footer-widget--dark';
+} else {
+    $widget_theme = 'c-footer-widget--light';
 }
 
-
 foreach ($columns as $i => $column) {
-    register_sidebar(array(
+    register_sidebar( array(
         'id' => 'footer-sidebar-' . ($i + 1),
-        'name' => sprintf(esc_html__( 'Footer Widgets Column %s', '_themename'), $i + 1),
-        'description' => esc_html__( 'Footer Widgets', '_themename'),
-        'before_widget' => '<section id="%1s" class="c-sidebar-widget u-margin-bottom-20 %2s">',
-        'after-widget' => '</section>',
-        'before-title' =>  '<h5>',
-        'after-title' => '</h5>'
-    ));
+        'name' => sprintf(esc_html__( 'Footer Widgets Column %s', '_themename' ), $i +1),
+        'description' => esc_html__( 'Footer widgets', '_themename' ),
+        'before_widget' => '<section id="%1$s" class="c-footer-widget ' . $widget_theme . ' %2$s">',
+        'after_widget' => '</section>',
+        'before_title' => '<h5>',
+        'after_title' => '</h5>'
+    ) );
 }
 
 add_action('widgets_init', '_themename_sidebar_widgets');
-
-?>
